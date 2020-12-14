3f8ccf30900bc8b8826aec7b3c0f7d74900999ed
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2020-12-14 23:01:07 +0700

    Initial Commit

---
+++ ShoesShopApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoesShopApp
+//
+//  Created by Nguyen Thanh Long on 12/14/20.
+//  Copyright © 2020 Nguyen Thanh Long. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
